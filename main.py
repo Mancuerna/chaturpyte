fe8058a59fe4ce51f292a65c2109ea7ad50728a7
esCFF <you@example.com>
2020-03-03 18:53:11 +0000

    rework

--- main.py
+++ main.py
@@ -1,67 +1,62 @@
 from Chaturbate import Chaturbate
 import queue
-import threading
-import colorama 
+import colorama
 from colorama import Fore, Style
 import time
 import datetime
 import os
-from pathlib import Path
+import threading
+
+
+MODELS = queue.Queue()
+PATH = os.path.dirname(os.path.abspath(__file__))
+ACTIVE_FAKE = {}
+TIME_FORMAT = '%H:%M:%S %m-%d-%Y'
+TICKS = 360
 
 colorama.init(strip=False)
 
-MODELS = queue.Queue()
-ACTIVE_RECORDINGS = []
-PATH = os.path.dirname(os.path.abspath(__file__))
-FILES = {}
+def waiting_captcha_reset():
+    waiting_time = 0
+    print(f'{Fore.RED}\u2620 {Fore.WHITE} Captcha error, waiting for response..')
+    while waiting_time < 600:
+        time.sleep(1)
+        waiting_time+=1
+        
 
-def get_model_list():
+def manager():
     [MODELS.put(model.strip()) for model in open(f'{PATH}/model_list.txt', 'r').readlines()]
-
-
-def get_active_recordings():
-    global ACTIVE_RECORDINGS
-    ACTIVE_RECORDINGS = [t.getName() for t in threading.enumerate()]
-    if ACTIVE_RECORDINGS[1::]:
-        for arco in ACTIVE_RECORDINGS[1::]:
-            file_size = str(round(get_file_size(FILES[arco][0]), 2))
-            start_at = FILES[arco][1].strftime("%H:%M:%S %m-%d-%Y")
-            duration = current_stream_duration(FILES[arco][1])                
-            print(f'{Fore.GREEN}\u2192 {Fore.WHITE}{arco}:{Fore.GREEN} Online {Fore.RED}\u25B6 {Fore.WHITE}Recording.. {file_size}Gb {duration}min start {start_at}')
-            
-            
-def get_file_size(file):
-    while True:
-        try:
-            return Path(file).stat().st_size/(1024*1024*1024)
-            break
-        except FileNotFoundError:
-            time.sleep(0.1)
-
-def current_stream_duration(start_time):
-    return str(round((datetime.datetime.now()-start_time).seconds/60, 2))
-            
-
-def record_models():
     while MODELS.empty() is False:
         model = MODELS.get()
-        if model not in ACTIVE_RECORDINGS:
-            ch = Chaturbate(model, PATH)
-            if ch.online_status() is False:
-                print(f'{Fore.RED}\u25CF {Fore.WHITE}{model}:{Fore.RED} Offline.{Style.RESET_ALL}')
-            else:
-                threading.Thread(target=ch.save_stream, name=model).start()
-                while not ch.output_stream:
-                    time.sleep(0.1)
-                FILES[model] = [ch.output_stream, datetime.datetime.now()]
-                
+        if model in ACTIVE_FAKE and ACTIVE_FAKE[model].isAlive() is False:
+            ACTIVE_FAKE.pop(model, None)
+        if model in ACTIVE_FAKE and ACTIVE_FAKE[model].isAlive() is True:
+            ACTIVE_FAKE[model].print_status()
+        if model not in ACTIVE_FAKE:
+            ch = Chaturbate(model, PATH, TIME_FORMAT)
+            ch.start()
+            while not ch.status and not ch.errors:
+                time.sleep(0.1)
+            if ch.status == 'offline':
+                ch.join()
+            elif ch.status == 'public' and model not in ACTIVE_FAKE:
+                ACTIVE_FAKE[ch.threadingID] = ch
+            elif ch.errors:
+                if ch.errors == 'captcha':
+                    waiting_captcha_reset()
+
 def main():
-    while True:
-        print(f'{Fore.BLUE}\u267B {Fore.WHITE}{datetime.datetime.now():%Y/%m/%d %H:%M:%S}')
-        get_model_list()
-        record_models()
-        get_active_recordings()
-        time.sleep(360)
+    try:
+        while True:
+            print(f'{Fore.BLUE}\u267B {Fore.WHITE}{datetime.datetime.now().strftime(TIME_FORMAT)}')
+            manager()
+            print(ACTIVE_FAKE)
+            print (f'Current Cams {threading.active_count()-1}')
+            time.sleep(TICKS)
+    except KeyboardInterrupt:
+        for t in ACTIVE_FAKE:
+            ACTIVE_FAKE[t].stop()
+        print(f'{Fore.YELLOW}bb{Style.RESET_ALL}')
 
 if __name__ == "__main__":
     main()
